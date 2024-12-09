e24becbc39efb493a870495d38a61a66601dccae
Marat <maratve2002@mail.ru>
2024-12-09 16:28:45 +0400

    next

---
+++ app/marat/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MaratPage = () => {
+    return (
+        <div>
+           <h1 className='text-center'>
+               Marat
+           </h1>
+        </div>
+    );
+};
+
+export default MaratPage;
---
+++ components/Menu.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Link from "next/link";
+import {ABOUT_PAGE, DONATE_PAGE, HOME_PAGE, MARAT_PAGE} from "@/utils/urls";
+
+const Menu = () => {
+    return (
+        <div>
+            <ul className="flex gap-4">
+                <li>
+                    <Link href={HOME_PAGE}>
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link href={ABOUT_PAGE}>
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link href={DONATE_PAGE}>
+                        Donate
+                    </Link>
+                </li>
+                <li>
+                    <Link href={MARAT_PAGE}>
+                        Marat
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Menu;
